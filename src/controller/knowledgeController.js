63bcfecf9794bc8328f9da1839c338298a8778b5
tannv6 <tannv6@gmail.com>
2023-03-18 16:53:44 +0700

    add root path

--- src/controller/knowledgeController.js
+++ src/controller/knowledgeController.js
@@ -7,6 +7,7 @@ const {
 let router = express.Router();
 
 const initKnowledgeRoutes = (app) => {
+  router.get("/", (req, res) => res.status(200).json("SERVER IS ON"));
   router.get("/knowledge", getAllKnowledge);
   router.post("/create", createNewKnowledge);
   return app.use("/", router);
