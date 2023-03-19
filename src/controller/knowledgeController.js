c23c8fa0c083362f8d36770e18be1233745c4a2d
tannv6 <tannv6@gmail.com>
2023-03-19 10:26:31 +0700

    test

--- src/controller/knowledgeController.js
+++ src/controller/knowledgeController.js
@@ -12,6 +12,12 @@ const initKnowledgeRoutes = (app) => {
   router.get("/", (req, res) =>
     res.status(200).json("SERVER IS ON 19.03.2023")
   );
+  router.get("/test", (req, res) => {
+    res.status(200).json({
+      data: [{ knowledge_name: "Tan", answer: "Love Minh" }],
+      status: "success",
+    });
+  });
   router.get("/knowledge", getAllKnowledge);
   router.post("/create", createNewKnowledge);
   router.put("/update", updateKnowledge);
